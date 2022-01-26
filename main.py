160f2bea19bb0e3f15c3c2d38c2400d07213aac2
Arturo Figueroa <arturofigueroa@swhkw2195.uni-trier.de>
2022-01-26 15:44:05 +0100

    comment code

--- main.py
+++ main.py
@@ -37,5 +37,5 @@ def predict():
     return response
 
 #Run local the app
-if __name__ == '__main__':
-   app.run(debug=True, host='0.0.0.0', port=9696)
+#if __name__ == '__main__':
+   #app.run(debug=True, host='0.0.0.0', port=9696)
